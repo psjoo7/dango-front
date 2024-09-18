25a84f223333a2ab52044a67e9ab10e64b11bd64
psjoo7 <jmin9480@naver.com>
2024-09-18 18:31:03 +0900

    login 페이지 url 변경

--- dango/src/App.js
+++ dango/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/member/login" element={<LoginPage />} />
       </Routes>
     </Router>
   );
