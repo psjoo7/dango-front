aec959d85aee9bf88475df128abf95978f15781c
psjoo7 <jmin9480@naver.com>
2024-10-05 02:19:09 +0900

    no message

--- dango/src/component/Forms/SignUpPageForm/SignUpPageForm.jsx
+++ dango/src/component/Forms/SignUpPageForm/SignUpPageForm.jsx
@@ -41,8 +41,8 @@ const SignUpPageForm = ({ className = "" }) => {
     try {
       // 서버로 회원가입 요청 전송
       const response = await axios.post(
-        // "https://scit45dango.site/api/member/join", // 서버 환경의 경우
-        "http://localhost:8888/api/member/join", // 로컬 환경의 경우
+        "https://scit45dango.site/api/member/join", // 서버 환경의 경우
+        // "http://localhost:8888/api/member/join", // 로컬 환경의 경우
         {
           userEmail: formData.email,
           userPassword: formData.userPassword,
@@ -55,8 +55,8 @@ const SignUpPageForm = ({ className = "" }) => {
       if (response.status === 201) {
         alert("회원가입에 성공했습니다.");
         const response = await axios.post(
-          // "https://scit45dango.site/api/member/login", // 서버 환경의 경우
-          "http://localhost:8888/api/member/login", // 로컬 환경의 경우
+          "https://scit45dango.site/api/member/login", // 서버 환경의 경우
+          // "http://localhost:8888/api/member/login", // 로컬 환경의 경우
           {
             userEmail: formData.email,
             userPassword: formData.userPassword,
@@ -221,16 +221,6 @@ const SignUpPageForm = ({ className = "" }) => {
             propFontWeight={700}
           />
         </div>
-
-        {/* <InputText
-          propWidth={"515px"}
-          propHeight={"38px"}
-          propBorderRadius={"13px"}
-          propLabelText={"전화번호"}
-          propLabelFontSize={"var(--font-body2)"}
-          propInputFontSize={"var(--font-body3)"}
-          propPlaceholder={"전화번호를 입력해 주세요."}
-        /> */}
       </div>
 
       <div className={styles.radios}>
--- dango/src/pages/Study/WordPage/WordPage.jsx
+++ dango/src/pages/Study/WordPage/WordPage.jsx
@@ -14,7 +14,8 @@ const WordPage = () => {
       const type = "단어";
 
       const response = await axios.post(
-        "http://localhost:8888/api/study/word",
+        // "http://localhost:8888/api/study/word",
+        "https://scit45dango.site/api/study/word",
         { level, userId, type }
       );
 
@@ -52,7 +53,8 @@ const WordPage = () => {
       const type = "단어";
 
       const response = await axios.post(
-        "http://localhost:8888/api/study/word",
+        // "http://localhost:8888/api/study/word",
+        "https://scit45dango.site/api/study/word",
         { level, userId, type }
       );
 
